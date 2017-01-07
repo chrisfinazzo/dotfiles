f68962a3c513e4c74881aeb85259aad3d32b6eef
chrisfinazzo <chris.finazzo@gmail.com>
2017-01-06 23:08:12 -0500

    Safety and Emacs redux

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 set -e
 
 function makeLinks() {
-	ln -fs ~/.bash_profile ~/Documents/Dotfiles/.bash_profile
-	ln -fs ~/.bash_prompt ~/Documents/Dotfiles/.bash_prompt
-	ln -fs ~/.gitconfig ~/Documents/Dotfiles/.gitconfig
-	ln -fs ~/.gitignore_global ~/Documents/Dotfiles/.gitignore_global
+	ln -s ~/.bash_profile ~/Documents/Dotfiles/.bash_profile
+	ln -s ~/.bash_prompt ~/Documents/Dotfiles/.bash_prompt
+	ln -s ~/.emacs.d/init.el ~/Documents/Dotfiles/.emacs.d/init.el
+	ln -s ~/.gitconfig ~/Documents/Dotfiles/.gitconfig
+	ln -s ~/.gitignore_global ~/Documents/Dotfiles/.gitignore_global
 	
 	read -rp "This may overwrite existing files. Are you sure? (y/n) " -n 1;
 	echo "";
