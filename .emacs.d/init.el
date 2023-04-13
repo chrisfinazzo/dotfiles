69bee791a8415188ac5c6706737400f286f2c7cd
chrisfinazzo <chris.finazzo@gmail.com>
2023-04-13 16:27:38 -0400

    Comment the file (so you don't do something stupid later)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,3 +9,16 @@
 
 ;; Disable autosave files
 (setq auto-save-default nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(cua-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
