ff8abdc1db4ff3d3eda0b8a92dd95df50da3d955
chrisfinazzo <chris.finazzo@gmail.com>
2024-06-16 21:25:38 -0400

    Make the setup script use Homebrew's prefix, update the Brewfile

--- setup.sh
+++ setup.sh
@@ -1,38 +1,38 @@
-#!/usr/local/bin/zsh
+#!/bin/bash
 
 set -e
 
 function makeLinks() {
   ln -s ~/Documents/Dotfiles/.zshrc ~/.zshrc
   ln -s ~/Documents/Dotfiles/.curlrc ~/.curlrc
+  ln -s ~/Documents/Dotfiles/.codespellrc ~/.codespellrc
   ln -s ~/Documents/Dotfiles/.wgetrc ~/.wgetrc
   ln -s ~/Documents/Dotfiles/.gitconfig ~/.gitconfig
   ln -s ~/Documents/Dotfiles/.gitignore_global ~/.gitignore_global
   ln -s ~/Documents/Dotfiles/.hushlogin ~/.hushlogin
   ln -s ~/Documents/Dotfiles/.bc ~/.bc
   ln -s ~/Documents/Dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
-  sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+  sudo ln -sfn $(brew --prefix)/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 }
 
-read -rp "Did you remember to install Homebrew? If not, do that first. Are you sure you want to continue? (y/n) " -n 1;
+echo "Did you remember to install Homebrew? If not, do that first. (y/n) " -n 1;
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    brew bundle
+    brew bundle && pipx ensurepath
   else
     open https://brew.sh
   fi;
 
-read -rp "Do you want to save a copy of your Brewfile? (y/n) " -n 1;
+echo "Do you want to save a copy of your Brewfile? (y/n) " -n 1;
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     ~/Desktop && brew bundle dump
   fi;
 
-read -rp "Did you remember to install Git? If not, do that now.
-This may overwrite existing files. Are you sure you want to continue? (y/n) " -n 1;
+echo "Almost done! Do you want to continue? (y/n) " -n 1;
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    makeLinks
-  else
-    brew install git
+    makeLinks && chsh -s /usr/local/bin/zsh
   fi;
+  
+  exec $SHELL && brew doctor
