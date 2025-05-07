4deb99bd71c424a9866e82aa7359215412878459
chrisfinazzo <chris.finazzo@gmail.com>
2025-05-07 10:07:01 -0400

    Aspell + emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,8 +7,33 @@
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 (setq scroll-step 1) ;; scroll smoothly
 
+;; Set ispell-program-name to aspell
+(setq ispell-program-name "aspell")
+
+;; Set aspell parameters
+(setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))
+
+;; Enable flyspell-mode for text and programming modes
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+
+;; For programming modes, only check comments
+(dolist (hook '(emacs-lisp-mode-hook
+                c-mode-hook
+                python-mode-hook))
+  (add-hook hook (lambda () (flyspell-prog-mode))))
+
+;; Key bindings for spellcheck
+(global-set-key (kbd "C-c i c") 'ispell-comments-and-strings)
+(global-set-key (kbd "C-c i d") 'ispell-change-dictionary)
+(global-set-key (kbd "C-c i f") 'flyspell-buffer)
+(global-set-key (kbd "C-c i r") 'flyspell-region)
+(global-set-key (kbd "C-c i b") 'ispell-buffer)
+(global-set-key (kbd "C-c i w") 'ispell-word)
+
 ;; Disable autosave files
 (setq auto-save-default nil)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
